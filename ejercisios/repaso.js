afdd7db684addad5425952f0b22baa21da876967
ccbenavides <ccbenavidesmontenegro@gmail.com>
2016-09-04 16:17:01 -0500

    christian

---
+++ ejercisios/repaso.js
@@ -0,0 +1,69 @@
+// type primitivos de javascript
+//String 
+//boolean
+//Number -> integers y floats
+//object -> array y object
+
+// undefined
+// NaN
+
+// defineme que es una variable
+var numero = 5;
+var cadena = "cadena";
+var boolean = false;
+var arreglo = ["agua",3,false,{}];
+var objecto = {
+    clave : "71433272",
+    nombre : "vimcemt",
+    mostrar_datos : function(){
+     console.log(this.clave + "-" +this.nombre)   
+    }
+};
+
+// palabras claves 
+// if 
+// else
+// for
+var array = [3,5,6,7];
+
+for (let index = 0; index < array.length; index++) {
+    // console.log(array[index]);    
+}
+
+// while
+var boleano = true;
+while(boleano){
+    boleano = false;
+}
+// function
+// return 
+function nombre_function(param){
+    console.log(param);
+    // contenido
+}
+nombre_function(4);
+// ** restricciones
+// 1 . un parametro no se puede declarar afuera como una variable 
+// 2 . si queremos retornar una valor debemos usar return 
+
+// var
+// let
+// const
+
+// typeof : averiguar que type es el elemento
+
+
+// class
+// new 
+// this
+
+
+
+
+
+
+
+
+
+
+
