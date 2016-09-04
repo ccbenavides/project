df99d3693b77a3d0668ebd762bc9680fb2baaa46
erikEBM <howl_bm96@hotmail.com>
2016-09-04 15:35:23 -0500

    nombre

--- ejercisios/name.js
+++ ejercisios/name.js
@@ -1,1 +1,83 @@
-console.log("algo");
+/*
+var area;
+var radio = 3;
+var PI = 3.14;
+
+function calculararea(parametro1){
+    area = PI * (parametro1*parametro1)
+    return area;
+}
+
+console.log(calculararea(radio));
+*/
+/*
+var uno = prompt("ingrese un numero");
+var dos = prompt("ingrese otro numero");
+
+if(uno === dos){
+    console.log("son iguales");
+
+} else {
+    console.log("son diferentes");
+}*/
+/*
+var uno = prompt("ingrese un numero");
+if(uno > 0){
+    console.log("es positivo");
+} else {
+    consoe.log("es negativo");
+}
+*/
+/*
+var uno = prompt("ingrese un numero");
+var dos = prompt("ingrese otro numero");
+
+if(uno % dos === 0){
+    console.log("son multiplos");
+} else {
+    console.log("no son multiplos");
+} */
+/*
+var uno = prompt("ingrese un muero");
+var dos = prompt("ingrese otro numero");
+
+if(uno > dos){
+    console.log("el primer numero es mayor");
+} else {
+    console.log("el segundo numero es mayor");
+}
+*/
+/*
+var uno = prompt("ingrese un muero");
+var dos = prompt("ingrese otro numero");
+
+if(uno > dos){
+    console.log("el primer numero es mayor");
+    } else if (dos > uno){
+
+    } else {
+        console.log("son iguales");
+    }
+    */
+    /*
+var uno = prompt("ingrese un muero");
+var dos = prompt("ingrese otro numero");
+
+if(uno > dos){
+    console.log(uno, dos);
+} else {
+    console.log(dos, uno);
+} */
+/*
+var uno = prompt("ingrese un muero");
+var dos = prompt("ingrese otro numero");
+var tres = prompt("ingrese otro numero");
+
+if(uno >= dos && uno >= tres){
+    console.log(uno, dos, tres);
+} else if (dos >= uno && uno >= tres && dos >= tres){
+    console.log(dos, uno, tres);
+} else  {
+    console.log(tres, dos, uno); 
+}
+  */
